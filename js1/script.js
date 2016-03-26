20137875655f032c4620bfc1e67b6a5329c147c7
Vitaliy Podrez <tamtamlg@gmail.com>
2016-03-27 00:40:38 +0200

    dz3-4

--- js1/script.js
+++ js1/script.js
@@ -4,31 +4,32 @@
 
 
 function pow(a, b) {
+	'use strict';
 	var result = a;
 	var firstResult = a;
-	if (b == 0) {
+	if (b === 0) {
 		result = 1;
-	} 
-	if (b == 1) {
+	}
+	if (b === 1) {
 		result = a;
 	}
-	if ((b > 0) && (b !== 1)) { 
-		for (var i = 1; i < b; i++) { 
+	if ((b > 0) && (b !== 1)) {
+		for (var i = 1; i < b; i++) {
 		result *= a;
 		}
 	}
-	if (b == -1) {
+	if (b === -1) {
 		result = 1/a;
 	}
-	if ((b < 0) && (b !== -1)) { 
-		for (var i = 1; i < (b*(-1)); i++) { 
+	if ((b < 0) && (b !== -1)) {
+		for (var i = 1; i < (b*(-1)); i++) {
 		firstResult *= a;
 		result = 1/firstResult;
 		}
 	}
 	console.log(result);
 	return (result);
-} 
+}
 
 var a = prompt ('Введите число, которое нужно возвести в степень', 0);
 var b = prompt ('Введите значение степени', 0);
---
+++ js3-4/script.js
@@ -0,0 +1,79 @@
+//Создать объект с методами, которые будут динамически генерировать DOM вот такой страницы. Это будет тест,
+// который мы будем разрабатывать в следующих заданиях. Сейчас вам нужно только динамически создать html,
+// методы должны храниться в одном объекте. Для того чтоб страница выглядела красиво можете использовать Bootstrap
+// и создавать DOM-элементы с готовыми бутстраповскими классами. Изначально на странице должен быть только <body>,
+// вызывая методы объекта нужно создать dom-элементы
+
+window.addEventListener('load', function fullLoad() {
+	'use strict';
+	var numQuestions,
+        numAnswers,
+	    mainObject = {
+	        create: function (tag, param, parentElement) {
+		        tag = document.createElement(tag);
+		        parentElement.appendChild(tag);
+		        tag.classList.add(param);
+	        },
+		
+	        inner: function (element, text) {
+		        element.innerHTML = text;
+	        },
+            
+            createInput: function (tag, text) {
+                tag.innerHTML = '<input type="checkbox">' + text;
+            }
+	    },
+        heading,
+        questions,
+        QuestionsLi;
+	
+	numQuestions = 3;
+    numAnswers = 3;
+    
+	mainObject.create('h1', 'heading', document.body);
+	heading = document.querySelector('h1');
+	mainObject.inner(heading, 'Тест по программированию');
+	
+	mainObject.create('ol', 'questions', document.body);
+	questions = document.querySelector('ol');
+	for (var i = 0; i < numQuestions; i++) {
+		mainObject.create('li', 'questionsItem', questions);
+	}
+	QuestionsLi = document.getElementsByClassName('questionsItem');
+	console.log(QuestionsLi);
+	mainObject.inner(QuestionsLi[0], 'Вопрос №1');
+	mainObject.inner(QuestionsLi[1], 'Вопрос №2');
+	mainObject.inner(QuestionsLi[2], 'Вопрос №3');
+    
+    
+    for (var i = 0; i < numAnswers; i++) {
+        mainObject.create('div', 'answers', QuestionsLi[0]);  
+    }
+    for (var i = 0; i < numAnswers; i++) {
+        mainObject.create('div', 'answers', QuestionsLi[1]);  
+    }
+    for (var i = 0; i < numAnswers; i++) {
+        mainObject.create('div', 'answers', QuestionsLi[2]);  
+    }
+    var InputArr = document.getElementsByClassName('answers');
+	for (var i = 0; i < InputArr.length; i++) {
+    InputArr[i].setAttribute('id', 'answer' + (i+1));
+    }
+    
+	mainObject.createInput(answer1, 'Вариант ответа №1');
+    mainObject.createInput(answer2, 'Вариант ответа №2');
+    mainObject.createInput(answer3, 'Вариант ответа №3');
+    
+    mainObject.createInput(answer4, 'Вариант ответа №1');
+    mainObject.createInput(answer5, 'Вариант ответа №2');
+    mainObject.createInput(answer6, 'Вариант ответа №3');
+    
+    mainObject.createInput(answer7, 'Вариант ответа №1');
+    mainObject.createInput(answer8, 'Вариант ответа №2');
+    mainObject.createInput(answer9, 'Вариант ответа №3');
+    
+    mainObject.create('div', 'btn', document.body);
+    var button = document.getElementsByClassName('btn');
+    button[0].setAttribute('id', 'submit');
+    submit.innerHTML = '<input type="submit" value="Проверить мои результаты">';
+});
