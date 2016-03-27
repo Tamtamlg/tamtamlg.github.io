88a49f03e8702d807853dcd1e5f6abb006314d6f
Vitaliy Podrez <tamtamlg@gmail.com>
2016-03-27 14:37:34 +0300

    fixed errors jslint

--- js3-4/script.js
+++ js3-4/script.js
@@ -7,28 +7,31 @@
 window.addEventListener('load', function fullLoad() {
 	'use strict';
 	var numQuestions,
-        numAnswers,
-	    mainObject = {
-	        create: function (tag, param, parentElement) {
-		        tag = document.createElement(tag);
-		        parentElement.appendChild(tag);
-		        tag.classList.add(param);
-	        },
-		
-	        inner: function (element, text) {
-		        element.innerHTML = text;
-	        },
-            
-            createInput: function (tag, text) {
-                tag.innerHTML = '<input type="checkbox">' + text;
-            }
+    numAnswers,
+    mainObject = {
+	    create: function (tag, param, parentElement) {
+		    tag = document.createElement(tag);
+		    parentElement.appendChild(tag);
+		    tag.classList.add(param);
 	    },
-        heading,
-        questions,
-        QuestionsLi;
+		
+	    inner: function (element, text) {
+		    element.innerHTML = text;
+	    },
+            
+      createInput: function (tag, text) {
+        tag.innerHTML = '<input type="checkbox">' + text;
+      }
+    },
+    heading,
+    questions,
+    QuestionsLi,
+    InputArr,
+    button,
+    i;
 	
 	numQuestions = 3;
-    numAnswers = 3;
+  numAnswers = 3;
     
 	mainObject.create('h1', 'heading', document.body);
 	heading = document.querySelector('h1');
@@ -36,44 +39,44 @@ window.addEventListener('load', function fullLoad() {
 	
 	mainObject.create('ol', 'questions', document.body);
 	questions = document.querySelector('ol');
-	for (var i = 0; i < numQuestions; i++) {
+	for (i = 0; i < numQuestions; i++) {
 		mainObject.create('li', 'questionsItem', questions);
 	}
 	QuestionsLi = document.getElementsByClassName('questionsItem');
-	console.log(QuestionsLi);
+	
 	mainObject.inner(QuestionsLi[0], 'Вопрос №1');
 	mainObject.inner(QuestionsLi[1], 'Вопрос №2');
 	mainObject.inner(QuestionsLi[2], 'Вопрос №3');
     
     
-    for (var i = 0; i < numAnswers; i++) {
-        mainObject.create('div', 'answers', QuestionsLi[0]);  
-    }
-    for (var i = 0; i < numAnswers; i++) {
-        mainObject.create('div', 'answers', QuestionsLi[1]);  
-    }
-    for (var i = 0; i < numAnswers; i++) {
-        mainObject.create('div', 'answers', QuestionsLi[2]);  
-    }
-    var InputArr = document.getElementsByClassName('answers');
-	for (var i = 0; i < InputArr.length; i++) {
-    InputArr[i].setAttribute('id', 'answer' + (i+1));
-    }
+  for (i = 0; i < numAnswers; i++) {
+    mainObject.create('div', 'answers', QuestionsLi[0]);
+  }
+  for (i = 0; i < numAnswers; i++) {
+    mainObject.create('div', 'answers', QuestionsLi[1]);
+  }
+  for (i = 0; i < numAnswers; i++) {
+    mainObject.create('div', 'answers', QuestionsLi[2]);
+  }
+  InputArr = document.getElementsByClassName('answers');
+  for (i = 0; i < InputArr.length; i++) {
+    InputArr[i].setAttribute('id', 'answer' + (i + 1));
+  }
     
-	mainObject.createInput(answer1, 'Вариант ответа №1');
-    mainObject.createInput(answer2, 'Вариант ответа №2');
-    mainObject.createInput(answer3, 'Вариант ответа №3');
+  mainObject.createInput(answer1, 'Вариант ответа №1');
+  mainObject.createInput(answer2, 'Вариант ответа №2');
+  mainObject.createInput(answer3, 'Вариант ответа №3');
     
-    mainObject.createInput(answer4, 'Вариант ответа №1');
-    mainObject.createInput(answer5, 'Вариант ответа №2');
-    mainObject.createInput(answer6, 'Вариант ответа №3');
+  mainObject.createInput(answer4, 'Вариант ответа №1');
+  mainObject.createInput(answer5, 'Вариант ответа №2');
+  mainObject.createInput(answer6, 'Вариант ответа №3');
     
-    mainObject.createInput(answer7, 'Вариант ответа №1');
-    mainObject.createInput(answer8, 'Вариант ответа №2');
-    mainObject.createInput(answer9, 'Вариант ответа №3');
+  mainObject.createInput(answer7, 'Вариант ответа №1');
+  mainObject.createInput(answer8, 'Вариант ответа №2');
+  mainObject.createInput(answer9, 'Вариант ответа №3');
     
-    mainObject.create('div', 'btn', document.body);
-    var button = document.getElementsByClassName('btn');
-    button[0].setAttribute('id', 'submit');
-    submit.innerHTML = '<input type="submit" value="Проверить мои результаты">';
+  mainObject.create('div', 'btn', document.body);
+  button = document.getElementsByClassName('btn');
+  button[0].setAttribute('id', 'submit');
+  submit.innerHTML = '<input type="submit" value="Проверить мои результаты">';
 });
