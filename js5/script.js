45d771c3977b3d649a5c0e3f8b67200036be18bf
Vitaliy Podrez <tamtamlg@gmail.com>
2016-04-01 13:30:11 +0300

    fixed errors jslint

---
+++ js5/script.js
@@ -0,0 +1,70 @@
+//Нужно сделать вот такой таймер (выбираете Stop Watch).
+//
+//Кнопка Start запускает таймер, так же нужно выводить милисекунды. После запуска она меняется на кнопку Pause, которая может приостанавливать таймер.
+//Кнопка Clear останавливает и обнуляет таймер.
+//Дизайн сделайте свой, используя Pure или Bootstrap.
+
+
+
+window.addEventListener('load', function fullLoad() {
+  'use strict';
+  var flag = true;
+  timerStart.addEventListener('click', function() {
+    if (flag) {
+      var secondTimer = setInterval(secondOn, 1000);
+      flag = !flag;
+    } else {
+      alert('timer stop');
+      }
+  });
+
+  var secondCount = 0;
+  second.innerHTML = '00';
+  var minuteCount = 0;
+  minute.innerHTML = '00';
+  var hourCount = 0;
+  hour.innerHTML = '00';
+  
+  function secondOn() {
+    secondCount++;
+    if (secondCount < 10) {
+      second.innerHTML = '0' + secondCount;
+    } else {
+      second.innerHTML = secondCount;
+    }
+    if (secondCount == 60) {
+      second.innerHTML = '00';
+      minuteOn();
+      secondCount = 0;
+    }
+  }
+  
+  function minuteOn() {
+    minuteCount++;
+    if (minuteCount < 10) {
+      minute.innerHTML = '0' + minuteCount;
+    } else {
+      minute.innerHTML = minuteCount;
+    }
+    if (minuteCount == 60) {
+      minute.innerHTML = '00';
+      hourOn();
+      minuteCount = 0;
+    }
+  }
+  
+  function hourOn() {
+    hourCount++;
+    if (hourCount < 10) {
+      hour.innerHTML = '0' + hourCount;
+    } else {
+      hour.innerHTML = hourCount;
+    }
+  }
+  
+  
+});
+
+
+
+
