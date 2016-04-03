47b6c1e74085b2d6642fd5ff4ccb6dcbb148419d
Vitaliy Podrez <tamtamlg@gmail.com>
2016-04-03 13:47:47 +0300

    del ms-timer

--- js5/script.js
+++ js5/script.js
@@ -8,32 +8,69 @@
 
 window.addEventListener('load', function fullLoad() {
   'use strict';
-  var flag = true;
-  timerStart.addEventListener('click', function() {
-    if (flag) {
-      var milliecondTimer = setInterval(milliSecondOn, 1);
-     // var secondTimer = setInterval(secondOn, 1000);
-      flag = !flag;
-    } else {
-      alert('timer stop');
+  var secondTimer,
+    timerStop,
+    timerStart,
+    timerClear,
+    second,
+    minute,
+    hour,
+    millisecond,
+    secondCount,
+    minuteCount,
+    hourCount,
+    milliSecondCount;
+  
+  timerStart = document.getElementById('timerStart');
+  timerStop = document.getElementById('timerStop');
+  timerClear = document.getElementById('timerClear');
+  second = document.getElementById('second');
+  minute = document.getElementById('minute');
+  hour = document.getElementById('hour');
+  millisecond = document.getElementById('millisecond');
+  
+  secondCount = 0;
+  minuteCount = 0;
+  hourCount = 0;
+  milliSecondCount = 0;
+  
+  second.innerHTML = '00';
+  minute.innerHTML = '00';
+  hour.innerHTML = '00';
+  millisecond.innerHTML = '0';
+  
+  timerStart.addEventListener('click', function () {
+    secondTimer = setInterval(secondOn, 1000);
+    timerStart.classList.add('btn-none');
+    timerStop.classList.remove('btn-none');
+  });
+  
+  timerStop.addEventListener('click', function () {
+    clearInterval(secondTimer);
+    timerStop.classList.add('btn-none');
+    timerStart.classList.remove('btn-none');
+  });
+  
+  timerClear.addEventListener('click', function () {
+    clearInterval(secondTimer);
+    secondCount = 0;
+    second.innerHTML = '00';
+    minuteCount = 0;
+    minute.innerHTML = '00';
+    hourCount = 0;
+    hour.innerHTML = '00';
+    milliSecondCount = 0;
+    millisecond.innerHTML = '0';
+    if (timerStart.classList.contains('btn-none')) {
+      timerStart.classList.remove('btn-none');
+      timerStop.classList.add('btn-none');
     }
   });
-
-  var secondCount = 0;
-  second.innerHTML = '00';
-  var minuteCount = 0;
-  minute.innerHTML = '00';
-  var hourCount = 0;
-  hour.innerHTML = '00';
-  var milliSecondCount = 0;
-  millisecond.innerHTML = '0';
-
+  
   function milliSecondOn() {
     milliSecondCount++;
-
-        millisecond.innerHTML = milliSecondCount;
-
-    if (milliSecondCount == 1000) {
+    millisecond.innerHTML = milliSecondCount;
+    if (milliSecondCount === 1000) {
       millisecond.innerHTML = '0';
       secondOn();
       milliSecondCount = 0;
@@ -47,7 +84,7 @@ window.addEventListener('load', function fullLoad() {
     } else {
       second.innerHTML = secondCount;
     }
-    if (secondCount == 60) {
+    if (secondCount === 60) {
       second.innerHTML = '00';
       minuteOn();
       secondCount = 0;
@@ -61,7 +98,7 @@ window.addEventListener('load', function fullLoad() {
     } else {
       minute.innerHTML = minuteCount;
     }
-    if (minuteCount == 60) {
+    if (minuteCount === 60) {
       minute.innerHTML = '00';
       hourOn();
       minuteCount = 0;
@@ -77,9 +114,4 @@ window.addEventListener('load', function fullLoad() {
     }
   }
 
-
 });
-
-
-
-
