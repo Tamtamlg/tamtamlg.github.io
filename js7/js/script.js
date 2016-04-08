39e94315f2e89e578450580ea02295f25c358f8c
Vitaliy Podrez <tamtamlg@gmail.com>
2016-04-08 16:36:23 +0300

    fixed bag in js7

--- js7/js/script.js
+++ js7/js/script.js
@@ -5,12 +5,15 @@ $(function () {
   
   $('ul').on('click', function (event) {
     var $active = $('.active');
-    var $activDiv = $($active.attr('href'));
+    var $activDiv = $('#tabs-1');
     
     $links.removeClass('active');
     if (!event.target.classList.contains('active')) {
       event.target.classList.add('active');
     }
+    
+    $active = $('.active');
+    $activDiv = $($active.attr('href'));
     
     $tabsItem.removeClass('tabs-item-active');
     if (!$activDiv.hasClass('tabs-item-active')) {
