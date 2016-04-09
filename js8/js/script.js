2d7a461019146e649cf740d6f54049b7acc0e5e0
Vitaliy Podrez <tamtamlg@gmail.com>
2016-04-09 21:41:37 +0300

    added js8

---
+++ js8/js/script.js
@@ -0,0 +1,28 @@
+$(function () {
+  'use strict';
+  var parentElem;
+  var inputContainer;
+  var $title = $('[data-title]');
+  var $btn = $('.btn');
+  
+  $title.on('mouseover', function (event) {
+    if (event.target.getAttribute('data-title')) {
+      var elemTargetId = event.target.getAttribute('id');
+      parentElem = document.createElement('div');
+      parentElem.classList.add('title-div');
+      parentElem.innerHTML = event.target.getAttribute('data-title');
+      inputContainer = document.getElementById(elemTargetId).parentNode;
+      inputContainer.appendChild(parentElem);
+    }
+  });
+  
+  $title.on('mouseout', function () {
+    if (parentElem) {
+      inputContainer.removeChild(parentElem);
+    }
+  });
+  
+  $btn.on('click', function () {
+    $title.trigger('mouseover');
+  });
+});
