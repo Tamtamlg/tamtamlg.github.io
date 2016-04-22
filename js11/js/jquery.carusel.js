4a39d4f0d220059680051a1763ee92a070d300ba
Vitaliy Podrez <tamtamlg@gmail.com>
2016-04-22 21:33:14 +0300

    fixed cursor in js11

--- js11/js/jquery.carusel.js
+++ js11/js/jquery.carusel.js
@@ -6,17 +6,46 @@
       $btnLeft = $('.btn-left'),
       $btnRight = $('.btn-right'),
       offset = 170,
-      currentOffset = 0;
+      currentOffset = 0,
+      $slideItems = $slide.find('li').length,
+      minOffset = -(($slideItems - 5) * offset),
+      maxOffset = 0;
+    
+    $btnLeft.addClass('start');
     
     $btnLeft.on('click', function () {
-      currentOffset -= 170;
-      $slide.animate({left: currentOffset + 'px'}, 500);
+      if (currentOffset !== minOffset) {
+        $btnRight.addClass('start');
+        currentOffset -= 170;
+        $slide.animate({left: currentOffset + 'px'}, 500);
+      }
+      if (currentOffset == minOffset) {
+        $(this).removeClass('start');
+      }
+      
     });
     
     $btnRight.on('click', function () {
-      currentOffset += 170;
-      $slide.animate({left: currentOffset + 'px'}, 500);
+      if (currentOffset !== maxOffset) {
+        $btnLeft.addClass('start');
+        currentOffset += 170;
+        $slide.animate({left: currentOffset + 'px'}, 500);
+      }
+      if (currentOffset == maxOffset) {
+        $(this).removeClass('start');
+      }
+      
+      
     });
+//    function toggleBtn () {
+//      if ($btnRight.hasClass('start')) {
+//        $btnRight.toggleClass('start');
+//      }
+//      if ($btnLeft.hasClass('start')) {
+//      $btnLeft.toggleClass('start');
+//      }
+//    }
+      
   };
   
 })(jQuery);
