540651e79706dd8d2bb675da813ba1b303274d83
Vitaliy Podrez <tamtamlg@gmail.com>
2016-09-25 22:24:01 +0300

    added contact

---
+++ contact/mail.php
@@ -0,0 +1,21 @@
+<?php
+$post = (!empty($_POST)) ? true : false;
+if($post) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$sub = $_POST["sub"];
+	$message = $_POST['message'];
+	$error = '';
+	if(!$name) {$error .= 'Укажите свое имя. ';}
+	if(!$email) {$error .= 'Укажите электронную почту. ';}
+	if(!$sub) {$error .= 'Укажите тему обращения. ';}
+	if(!$message || strlen($message) < 1) {$error .= 'Введите сообщение. ';}
+	if(!$error) {
+		$address = "tamtamlg@gmail.com";
+		$mes = "Имя: ".$name."\n\nТема: " .$sub."\n\nСообщение: ".$message."\n\n";
+		$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+		if($send) {echo 'OK';}
+	}
+	else {echo '<div class="err">'.$error.'</div>';}
+}
+?>
