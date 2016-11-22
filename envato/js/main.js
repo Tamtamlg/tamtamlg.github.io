a5a4fad1128a1cca3aec5a1056570ffe1fbf2556
Vitaliy Podrez <tamtamlg@gmail.com>
2016-11-23 01:12:41 +0200

    added envato

---
+++ envato/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  
+  // инициализируем слайдер в сайдбаре
+  $('.flexslider').flexslider({
+    directionNav: false,
+    animation: "slide",
+    controlNav: false,
+    slideshow: true
+  });
+  
+});
