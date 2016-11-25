b7c33280e6a47c5ebbaf95a6df50a9d29ec3eace
Vitaliy Podrez <tamtamlg@gmail.com>
2016-11-26 00:03:03 +0200

    added js

--- envato/js/main.js
+++ envato/js/main.js
@@ -1,11 +1,20 @@
-$(function() {
+$(function () {
+  'use strict';
+  var scrollBtn = $('.scroll-btn'),
+      header = $('header');
   
-  // инициализируем слайдер в сайдбаре
-  $('.flexslider').flexslider({
-    directionNav: false,
-    animation: "slide",
-    controlNav: false,
-    slideshow: true
+  //scrollTop
+  scrollBtn.click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: header.offset().top}, 2000);
+  });
+
+  
+  //accordion
+  var plus = $('.schedule-plus');
+  plus.click(function () {
+    var li = $(this).parent('li');
+      li.toggleClass('accordion-active');
   });
   
 });
