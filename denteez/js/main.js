08c17af05bcbe16365b4d9f18d7a729d0b22c34e
Vitaliy Podrez <tamtamlg@gmail.com>
2016-12-18 21:47:48 +0200

    added new token

--- denteez/js/main.js
+++ denteez/js/main.js
@@ -8,7 +8,7 @@ $(function() {
   function doRequest() {
       $.ajax({
           type: "GET",
-          headers: {"Authorization": "0db652546478e50858229ac61e3afc61da0deefa"},
+          headers: {"Authorization": "cc8ce950677315687c234ab23d78e4d40c8f3dec"},
           url: "http://504080.com/api/v1/services/categories",
           dataType: "json",
           success: function (data) {
