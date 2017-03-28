12d0d2b08959d3532f4e3c45c25906157de1f0a7
Vitaliy Podrez <tamtamlg@gmail.com>
2017-03-28 22:02:41 +0300

    added main menu

--- dontpanic/js/main.js
+++ dontpanic/js/main.js
@@ -1,9 +1,23 @@
+/*global $*/
 $(function () {
   'use strict';
   
+  // подключение слайдера
   $('.flexslider').flexslider({
     animation: "slide",
     directionNav: false
+  });
+  
+  // меню
+  var flag = false;
+  $('.bars').click(function () {
+    var menu = $('.main-menu');
+    if (!flag) {
+      menu.animate({right: 0}, 500);
+    } else {
+      menu.animate({right: '-300px'}, 500);
+    }
+    flag = !flag;
   });
   
 });
