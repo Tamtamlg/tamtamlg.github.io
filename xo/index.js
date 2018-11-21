590a7614f6b12bf4901958039aed3fed3298d663
Tamtamlg <tamtamlg@gmail.com>
2018-11-21 07:59:42 +0200

    remove comments

--- xo/index.js
+++ xo/index.js
@@ -1,35 +1,34 @@
-/*global getWinner*/
-window.addEventListener('load', function fullLoad() { // выполнять скрипт после полной загрузки документа
+window.addEventListener('load', function fullLoad() {
   'use strict';
-  var startNewGame = document.querySelector('.startNewGame'); // переменной присваиваем элемент с классом .startNewGame
-  var cells = document.querySelectorAll('.cell'); // переменной присваиваем элементы с классом .cell (получаем массив)
-  var field = document.querySelector('.field'); // переменной присваиваем элемент с классом .field
-  var change = true; // переменная для чередования х и о
-  startNewGame.addEventListener('click', function clickButton() { // вешаем обработчик на кнопку новой игры
+  var startNewGame = document.querySelector('.startNewGame');
+  var cells = document.querySelectorAll('.cell');
+  var field = document.querySelector('.field');
+  var change = true;
+  startNewGame.addEventListener('click', function clickButton() {
     var i;
-    for ( i = 0; i < cells.length; i++) { // перебираем все элементы с классом .cell
-      cells[i].classList.remove('x'); // удаляем класс .х
-      cells[i].classList.remove('o'); // удаляем класс .о
+    for ( i = 0; i < cells.length; i++) {
+      cells[i].classList.remove('x');
+      cells[i].classList.remove('o');
     }
-    document.querySelector('.winner-message').innerHTML = ''; // удаляем победное сообщение
-    change = true; // переинициализируем переменную, чтобы при новой игре ходил х
+    document.querySelector('.winner-message').innerHTML = '';
+    change = true;
   });
-  field.addEventListener('click', function clickCell(el) { // вешаем обработчик на элемент с классом .field, т.к. он родительский для всех .cell (используем всплытие)
-    if (el.target.classList.contains('cell') && // имеет ли элемент, на который кликнули, класс .cell
-      !el.target.classList.contains('x') && // не записан ли в элемент класс .х
-      !el.target.classList.contains('o')) { // не записан ли в элемент класс .о
-      if (!getWinner()) { // если не сработал скрипт getWinner
-        if (change) { // если change = true
-          el.target.classList.add('x'); // элементу,на который кликнули, присвоить класс .х
+  field.addEventListener('click', function clickCell(el) {
+    if (el.target.classList.contains('cell') &&
+      !el.target.classList.contains('x') &&
+      !el.target.classList.contains('o')) {
+      if (!getWinner()) {
+        if (change) {
+          el.target.classList.add('x');
         } else {
-          el.target.classList.add('o'); // элементу,на который кликнули, присвоить класс .о
+          el.target.classList.add('o');
         }
-        change = !change; // поменять значение change на false
-        if (getWinner() === 'x') { // если getWinner возвращает х
-          document.querySelector('.winner-message').innerHTML = 'Крестик победил'; // выводим победное сообщение
+        change = !change;
+        if (getWinner() === 'x') {
+          document.querySelector('.winner-message').innerHTML = 'Крестик победил';
         }
-        if (getWinner() === 'o') { // если getWinner возвращает о
-          document.querySelector('.winner-message').innerHTML = 'Нолик победил'; // выводим победное сообщение
+        if (getWinner() === 'o') {
+          document.querySelector('.winner-message').innerHTML = 'Нолик победил';
         }
       }
     }
